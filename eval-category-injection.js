9d9e5398d7712cd9297ccd7114fede9d71603ecc
archanam123 <mohite.archi@gmail.com>
2022-05-11 12:33:13 +0530

    Add files via upload

---
+++ eval-category-injection.js
@@ -0,0 +1,29 @@
+$.get(reLoadHead, opt).done(function (data) {
+            var headers = eval(data);
+function getCalendarData(param) {
+        var data = 234;
+        $.get(url, { startDate: '2016-8-28', endDate: '2016-10-8', id: 234 }).done(function (data) {
+            var evts = eval(data);
+            initCalendar(evts);
+            $(""#calendarModal"").modal('show');
+        }).fail(function (def) {
+            alert(""fail"");
+        })
+    }
+$.get(canlendarUrl, { startDate: startDate, endDate: endDate, id: id }).done(function (data) {
+        var evts = eval(data);
+$.get(canlendarUrl, { startDate: startDate, endDate: endDate, id: id }).done(function (data) {
+        var evts = eval(data);
+$.get(canlendarUrl, { startDate: startDate, endDate: endDate, id: id }).done(function (data) {
+        var evts = eval(data);
+$.get(viewDataUrl, { SelectedMonth: options.SelectedMonth, SelectedYear: options.SelectedYear, currentEnterpriseID: 'scv_Dev_ADMIN', IndustryGroupID: IndustryGroupID, intLocationID: Location, intVisitTypeID: 0, intVisitCategoryID: 0, SysVisitPrepStatusID: VisitTrackerStatus, DeliveryCenterID: -1, }).done(function (data) {
+            var initDate = $(""#initDate"").val();
+            var evts = eval(data);"
+$.get(reLoadHead, opt).done(function (data) {
+            var headers = eval(data);"
+function getViewData(options, flag) {
+        var initDate = $(""#initDate"").val();
+        $(""html"").mLoading('show');
+        $.get(viewDataUrl, options).done(function (data) {
+            var evts = eval(data);"
+
